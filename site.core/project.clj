68a92faa9156a04ea973e1135b988a5d1510bdc3
Christian Stjernberg <christian@stjernberg.com>
2023-03-11 22:24:38 +0100

    Add new clojurescript base site

---
+++ site.core/project.clj
@@ -0,0 +1,38 @@
+(defproject site.core "0.1"
+  :description "Webpage"
+  :url "https://christian.stjernberg.com/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v20.html"}
+
+  :min-lein-version "2.7.1"
+
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.11.4"]
+                 [cljsjs/react "17.0.2-0"]
+                 [cljsjs/react-dom "17.0.2-0"]
+                 [reagent "1.1.1"]
+                 [lein-tailwind "0.1.2"]] 
+
+  :source-paths ["src"]
+
+  :plugins [[lein-tailwind "0.1.2"]]
+
+  :hooks [leiningen.tailwind]
+  
+  :tailwind {:tailwind-dir "resources/public/css/tailwind" 
+             :output-dir   "resources/public/css"
+             :tailwind-config  "tailwind.config.js" ;; tailwind.config.js is the default value 
+             :styles [{:src "main.css"
+                       :dst "main.css"}]}
+
+  :aliases {"fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+            "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]}
+
+  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.2.17"]
+                                  [org.slf4j/slf4j-nop "1.7.30"]
+                                  [com.bhauman/rebel-readline-cljs "0.1.4"]]
+
+                   :resource-paths ["target"]
+                   ;; need to add the compiled assets to the :clean-targets
+                   :clean-targets ^{:protect false} ["target"]}})
+
---
+++ site.core/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './resources/public/index.html',
+    './resources/public/cljs-out/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
