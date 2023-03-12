5227931c67eaa256c87e0df1aae964268e9e0cb3
Christian Stjernberg <christian@stjernberg.com>
2023-03-12 19:46:18 +0100

    Improve layout and add instagram icon

--- site.core/src/site/core.cljs
+++ site.core/src/site/core.cljs
@@ -4,27 +4,27 @@
    [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]))
 
-(println "This text is printed from src/site/core.cljs. Go ahead and edit it and see reloading in action. ")
-
 ;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom {:text "Hello world!"}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
-(defn navbar []
-  [:nav {:class "flex justify-between bg-black text-white block"}
-   [:div {:href "" :class "p-3"} "Christian Stjernberg"]
-   [:div {:class ""}
-    [:a {:href "https://github.com/christian-stj/" :class "fa fa-github hover:scale-125 transition p-4"}]
-    [:a {:href "https://www.linkedin.com/in/christian-stj/" :class "fa fa-linkedin hover:scale-125 transition p-4"}]]])
-
-
 ; Styles
 (def header "text-left text-xl md:text-3xl box-decoration-clone text-black bg-white px-2")
 (def header-big "shrink text-left text-2xl md:text-4xl box-decoration-clone text-black bg-white px-2")
 (def project "p-6 bg-neutral-900 mt-6 rounded mb-6 shadow-sm shadow-neutral-900")
-(def global "font-mono text-sm md:text-base")
+(def img "my-3 shadow-lg rounded-sm")
+(def global "font-mono text-sm md:text-base scroll-auto")
+
+; Navbar
+(defn navbar []
+  [:nav {:class "flex justify-between bg-black text-white block"}
+   [:div {:href "" :class "p-3"} "Christian Stjernberg"]
+   [:div {:class "hidden sm:flex"} 
+    [:a {:href "https://www.instagram.com/christians_fotografi/" :class "fa fa-instagram hover:scale-125 transition p-4"}]
+    [:a {:href "https://github.com/christian-stj/" :class "fa fa-github hover:scale-125 transition p-4"}]
+    [:a {:href "https://www.linkedin.com/in/christian-stj/" :class "fa fa-linkedin hover:scale-125 transition p-4"}]]])
 
 ; Main app
 (defn app []
@@ -32,9 +32,10 @@
    ;; Top
    [navbar]
    ;; Body
-   [:div {:class "flex justify-center text-white m-3"}
+   [:div {:class "flex flex-row justify-center text-white m-3"}
     ;; Main Content
-    [:div {:class "sm:max-w-xl max-w-xs pt-6"}
+    [:div {:class "hidden md:flex basis-1/4"}]
+    [:div {:class "md:basis-2/4 center max-w-xs md:max-w-none pt-6"}
      [:div {:class "mb-10"}
       [:span {:class header-big} "About me"]
       [:img {:class "float-right max-w-xs w-1/2 sm:w-1/4 rounded-full border border-neutral-900 m-5" :src "./img/portrait.jpeg"}]
@@ -45,7 +46,7 @@
 
      ;; Projects
      [:span {:class header-big} "My projects"]
-     [:div 
+     [:div
      ;; This is where projects go
       [:div {:class project}
        [:span {:class header} "/"]
@@ -56,7 +57,7 @@
       [:div {:class project}
        [:span {:class header} "Bee Game"]
        [:a {:href "https://github.com/christian-stj/bee-game"}
-        [:img {:class "my-3 shadow-lg" :src "./img/bee_game_screens.png"}]]
+        [:img {:class img :src "./img/bee_game_screens.png"}]]
        [:p "This game was made in one evening after a little bee had visited our ice-cream during lunch. 
         The bee had some ice-cream stuck on it and chilled out on my hand for a few minutes while cleaning it out. 
         In the game the bee must escape the falling stracciatella ice-cream for as long as possible. 
@@ -64,20 +65,20 @@
       [:div {:class project}
        [:span {:class header} "GitViz - a GitHub Visualization tool"]
        [:a {:href "https://github.com/christian-stj/Visualizing-GitHub"}
-        [:img {:class "my-3 shadow-lg" :src "./img/gitviz1.png" :alt "GitHub Visualization - Small repo"}]]
+        [:img {:class img :src "./img/gitviz1.png" :alt "GitHub Visualization - Small repo"}]]
        [:p "For our bachelor thesis we developed a visualization tool that visualizes some GitHub data and metadata.
         The graph in the middle shows all repo files and directories as nodes with color and size varying depending on the
         distance from the root. The visualization can be dragged and scrolled and the nodes can also be dragged around.
         To the left there are two menus, one where you can go to different commits and see the file tree at that point in time and
         one where you can see different issues."]
-        [:a {:href "https://github.com/christian-stj/Visualizing-GitHub"}
-         [:img {:class "my-3 shadow-lg" :src "./img/gitviz2.png" :alt "GitHub Visualization - BIG repo"}]]
-         [:p "On encouragement from our supervisor we added dark mode because these are a must in modern websites.
+       [:a {:href "https://github.com/christian-stj/Visualizing-GitHub"}
+        [:img {:class img :src "./img/gitviz2.png" :alt "GitHub Visualization - BIG repo"}]]
+       [:p "On encouragement from our supervisor we added dark mode because these are a must in modern websites.
         As you can see on the image above the tool did not handle huge repositories well."]]
       [:div {:class project}
        [:span {:class header} "Todo List App"]
-       [:img {:class "my-3 shadow-lg" :src "./img/todo-list.png"}]
-       [:p "Full-stack project for an internet programming course, developed together with " 
+       [:img {:class img :src "./img/todo-list.png"}]
+       [:p "Full-stack project for an internet programming course, developed together with "
         [:a {:class "text-gray-400" :href "https://github.com/mansand1" :target "_blank"} "@mansand1"]
         " The application was developed with Vue front-end using node backend featuring a nice ORM SQLite database.
         Users could log in and their passwords were salted and hashed and stored in the database together
@@ -85,33 +86,34 @@
         local machine. "]]
       [:div {:class project}
        [:span {:class header} "Tiny Guitar Hero"]
-       [:img {:class "my-3 shadow-lg" :src "./img/guitar-hero-game.jpg"}]
+       [:img {:class img :src "./img/guitar-hero-game.jpg"}]
        [:p "For a project in embedded programming, me and "
         [:a {:class "text-gray-400" :href "https://github.com/theoahfeldt" :target "_blank"} "@theoahfeldt"]
         " developed a Guitar Hero replica. The program was developed for a PIC32 microcontroller using an IO shield and some external buttons
-connected to hardware pins on the chip.
-As you can see on the tiny screen in the image above the player is presented with some dots running
-down the screen where the player must
-press the right combination of buttons and the strum button att the right time to score.
-On the screen above, the player seems to have been a bit early in strumming as is displayed on the screen."]
-       [:img {:class "my-3 shadow-lg" :src "./img/guitar.jpg"}]
+        connected to hardware pins on the chip.
+        As you can see on the tiny screen in the image above the player is presented with some dots running
+        down the screen where the player must
+        press the right combination of buttons and the strum button att the right time to score.
+        On the screen above, the player seems to have been a bit early in strumming as is displayed on the screen."]
+       [:img {:class img :src "./img/guitar.jpg"}]
        [:p "The entire thing was packed in a flaming hot red case and gathered much interest in the project conference where
-students got to try out each other's projects. The tiny screen was placed inside the little rectangular slot at the side
-of the guitar neck and a speaker was connected to it playing som terribly offbeat square waves.
-Of course, no one could beat my high-score in the game."]]
+        students got to try out each other's projects. The tiny screen was placed inside the little rectangular slot at the side
+        of the guitar neck and a speaker was connected to it playing som terribly offbeat square waves.
+        Of course, no one could beat my high-score in the game."]]
       [:div {:class project}
        [:span {:class header} "Crappy Chess"]
        [:a {:href "https://github.com/christian-stj/crappy-chess"}
-        [:img {:class "my-3 shadow-lg" :src "./img/chess.png"}]]
+        [:img {:class img :src "./img/chess.png"}]]
        [:p "Our first \"major\" (3 weeks) project in school at the end of first year. A chess game developed with GoLang using game library Ebiten.
-The players took turns to move
-their pieces to any legal square and the game ended with the capture of the opposing king. The rules castling, en passant and
-pawn promotion as well as check and checkmate were not implemented in the game, hence the \"crappyness\"."]]]]
-    
+        The players took turns to move
+        their pieces to any legal square and the game ended with the capture of the opposing king. The rules castling, en passant and
+        pawn promotion as well as check and checkmate were not implemented in the game, hence the \"crappyness\"."]]]]
+
     ;; Sidebar
-        ;; [:div {:class "hidden text-xs md:flex sticky top-20 self-start ml-10"}
-        ;;  [:p "Check out my photography blog at " [:a {:href "https://www.instagram.com/christians_fotografi/"} "@christians_fotografi"]]]
-        ]
+    [:div {:class "hidden md:flex basis-1/4 self-start justify-center"} 
+    ;;  [:div {:class "mt-20 w-24 bg-neutral-900 rounded p-2 space-y-7"}
+    ;;   [:div ]
+     ]]
 
 
    ;; Footer 
