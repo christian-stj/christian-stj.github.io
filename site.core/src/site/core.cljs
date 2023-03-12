6b762c00c7d7e4fbfebd9857e0e2852eff29c732
Christian Stjernberg <christian@stjernberg.com>
2023-03-12 21:08:07 +0100

    Add snake content

--- site.core/src/site/core.cljs
+++ site.core/src/site/core.cljs
@@ -15,6 +15,7 @@
 (def header-big "shrink text-left text-2xl md:text-4xl box-decoration-clone text-black bg-white px-2")
 (def project "p-6 bg-neutral-900 mt-6 rounded mb-6 shadow-sm shadow-neutral-900")
 (def img "my-3 shadow-lg rounded-sm")
+(def social "fa hover:scale-125 transition p-4 visited:text-neutral-400")
 (def global "font-mono text-sm md:text-base scroll-auto")
 
 ; Navbar
@@ -22,9 +23,9 @@
   [:nav {:class "flex justify-between bg-black text-white block"}
    [:div {:href "" :class "p-3"} "Christian Stjernberg"]
    [:div {:class "hidden sm:flex"} 
-    [:a {:href "https://www.instagram.com/christians_fotografi/" :class "fa fa-instagram hover:scale-125 transition p-4"}]
-    [:a {:href "https://github.com/christian-stj/" :class "fa fa-github hover:scale-125 transition p-4"}]
-    [:a {:href "https://www.linkedin.com/in/christian-stj/" :class "fa fa-linkedin hover:scale-125 transition p-4"}]]])
+    [:a {:href "https://www.instagram.com/christians_fotografi/" :target "_blank" :class ["fa-instagram" social]}]
+    [:a {:href "https://github.com/christian-stj/" :target "_blank" :class ["fa-github" social]}]
+    [:a {:href "https://www.linkedin.com/in/christian-stj/" :target "_blank" :class ["fa-linkedin" social]}]]])
 
 ; Main app
 (defn app []
@@ -38,7 +39,7 @@
     [:div {:class "md:basis-2/4 center max-w-xs md:max-w-none pt-6"}
      [:div {:class "mb-10"}
       [:span {:class header-big} "About me"]
-      [:img {:class "float-right max-w-xs w-1/2 sm:w-1/4 rounded-full border border-neutral-900 m-5" :src "./img/portrait.jpeg"}]
+      [:img {:class "float-right w-1/2 md:w-1/3 xl:w-1/4 rounded-full border border-neutral-900 m-5" :src "./img/portrait.jpeg"}]
       [:div {:class "px-2 my-3"}
        [:p  "Hej! I'm a Computer Science student at KTH currently doing my thesis within the field of cybersecurity.
              I'm involved with multiple different non-profit organizations and work part time as an event technician.
@@ -55,8 +56,13 @@
         I used Reagent and Tailwind for layout and styling and Leiningen and Figwheel as build/dev tools. 
         The site got a complete new style and it is now also easier to expand and add to the site."]]
       [:div {:class project}
+       [:span {:class header} "cljs-snake"]
+       [:a {:href "http://christian.stjernberg.com/cljs-snake/" :target "_blank"}
+        [:img {:class img :src "./img/snake.png"}]] 
+       [:p "A simple snake game project I made to test out reagent and web programming with clojurescript."]]
+      [:div {:class project}
        [:span {:class header} "Bee Game"]
-       [:a {:href "https://github.com/christian-stj/bee-game"}
+       [:a {:href "https://github.com/christian-stj/bee-game" :target "_blank"}
         [:img {:class img :src "./img/bee_game_screens.png"}]]
        [:p "This game was made in one evening after a little bee had visited our ice-cream during lunch. 
         The bee had some ice-cream stuck on it and chilled out on my hand for a few minutes while cleaning it out. 
@@ -64,14 +70,14 @@
         When struck, the bee is covered with ice-cream and the score is shown on top."]]
       [:div {:class project}
        [:span {:class header} "GitViz - a GitHub Visualization tool"]
-       [:a {:href "https://github.com/christian-stj/Visualizing-GitHub"}
+       [:a {:href "https://github.com/christian-stj/Visualizing-GitHub" :target "_blank"}
         [:img {:class img :src "./img/gitviz1.png" :alt "GitHub Visualization - Small repo"}]]
        [:p "For our bachelor thesis we developed a visualization tool that visualizes some GitHub data and metadata.
         The graph in the middle shows all repo files and directories as nodes with color and size varying depending on the
         distance from the root. The visualization can be dragged and scrolled and the nodes can also be dragged around.
         To the left there are two menus, one where you can go to different commits and see the file tree at that point in time and
         one where you can see different issues."]
-       [:a {:href "https://github.com/christian-stj/Visualizing-GitHub"}
+       [:a {:href "https://github.com/christian-stj/Visualizing-GitHub" :target "_blank"}
         [:img {:class img :src "./img/gitviz2.png" :alt "GitHub Visualization - BIG repo"}]]
        [:p "On encouragement from our supervisor we added dark mode because these are a must in modern websites.
         As you can see on the image above the tool did not handle huge repositories well."]]
@@ -102,7 +108,7 @@
         Of course, no one could beat my high-score in the game."]]
       [:div {:class project}
        [:span {:class header} "Crappy Chess"]
-       [:a {:href "https://github.com/christian-stj/crappy-chess"}
+       [:a {:href "https://github.com/christian-stj/crappy-chess" :target "_blank"}
         [:img {:class img :src "./img/chess.png"}]]
        [:p "Our first \"major\" (3 weeks) project in school at the end of first year. A chess game developed with GoLang using game library Ebiten.
         The players took turns to move
